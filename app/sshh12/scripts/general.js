5dee65ed5db4ce11a0a410069e3cfe2ed89f18f9
Shrivu Shankar <shrivu1122@gmail.com>
2016-09-03 12:18:43 -0500

    Now Works with Migrated Backend

--- app/sshh12/scripts/general.js
+++ app/sshh12/scripts/general.js
@@ -1,6 +1,6 @@
 //Global Functions and Stuff
 
-var SVR_ADDRESS = "http://sshh12.com:8338/";
+var SVR_ADDRESS = "https://cfisdapi.herokuapp.com/";
 //var SVR_ADDRESS = "http://10.0.0.2:8338/";
 
 function createDocElement(name, bodytext) { //Magical HTML Parser Object
