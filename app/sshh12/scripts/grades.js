d94bcd3db939976362323415c0c2fbe8e27c2584
Shrivu Shankar <shrivu1122@gmail.com>
2016-11-22 11:29:45 -0600

    Added the Ablility to See ReportCard Grades

--- app/sshh12/scripts/grades.js
+++ app/sshh12/scripts/grades.js
@@ -24,6 +24,22 @@ function toggleVisibility(name) {
     }
 }
 
+function letterToColor(lettr){
+  if (lettr == 'A') {
+      return "balanced";
+  } else if (lettr == 'B') {
+      return "energized";
+  } else if (lettr == 'C') {
+      return "assertive";
+  } else if (lettr == 'D') {
+      return "royal";
+  } else if (lettr == 'U') {
+      return "calm";
+  } else {
+      return "dark";
+  }
+}
+
 function ClassGradeItem(subject, percent, lettr, ident){
 
   if (isUndefined(percent)) {
@@ -40,19 +56,7 @@ function ClassGradeItem(subject, percent, lettr, ident){
   this.ident = ident;
   this.subject = subject;
 
-  if (this.lettr == 'A') {
-      this.badge = "balanced";
-  } else if (this.lettr == 'B') {
-      this.badge = "energized";
-  } else if (this.lettr == 'C') {
-      this.badge = "assertive";
-  } else if (this.lettr == 'D') {
-      this.badge = "royal";
-  } else if (this.lettr == 'U') {
-      this.badge = "calm";
-  } else {
-      this.badge = "dark";
-  }
+  this.badge = letterToColor(this.lettr);
 
   this.getHTML = function(){
     return "<a class=\"item\"><i style='text-align: left' class='icon super-chevron-down'></i>&nbsp<b ontouchstart=\"toggleVisibility('" + this.ident + "');\">" + this.subject +
@@ -75,25 +79,34 @@ function AssignmentGradeItem(subject, name, percent, lettr, ident){
   this.subject = subject;
   this.ident = ident;
 
-  if (this.lettr == 'A') {
-      this.badge = "balanced";
-  } else if (this.lettr == 'B') {
-      this.badge = "energized";
-  } else if (this.lettr == 'C') {
-      this.badge = "assertive";
-  } else if (this.lettr == 'D') {
-      this.badge = "royal";
-  } else if (this.lettr == 'U') {
-      this.badge = "calm";
-  } else {
-      this.badge = "dark";
-  }
+  this.badge = letterToColor(this.lettr);
 
   this.getHTML = function(){
     return "<a style=\"display: none; font-size: 14px; color: #9c9c9c;\" class=\"item " + this.ident + "\">&nbsp;" + this.name +
            "<span style=\"font-size: 15px;\" ontouchstart=\"showStats('" + this.subject + "','" + this.name + "', '" + this.percent.replace("%", "") + "')\" class=\"badge badge-" + this.badge +
            "\">" + this.percent + "</span></a>";
 	};
+
+}
+
+function ReportCardItem(classname, averages){
+
+  this.classname = classname;
+  this.averages = averages;
+
+  this.getHTML = function(){
+    var avgHTML = "";
+    for(var m in this.averages){
+      var avg = this.averages[m].average;
+      var lettr = this.averages[m].letter;
+      if(avg.length < 1){
+        avg = "?";
+        lettr = "U";
+      }
+      avgHTML += "<div class=\"col\"><button class=\"button button-block button-" + letterToColor(lettr) + "\">" + avg + "</button></div>";
+    }
+    return "<a class=\"item\" href=\"#\"><b>" + this.classname + "</b><div class=\"row\">" + avgHTML + "</div></a>";
+  };
 
 }
 
@@ -105,9 +118,9 @@ function setFromClassworkJson(json){
   var classworkKeys = Object.keys(json);
   classworkKeys.sort(
       function(a, b) {
-          return json[a]["name"].toLowerCase() > json[b]["name"].toLowerCase() ? 1 : -1;
+          return json[a].name.toLowerCase() > json[b].name.toLowerCase() ? 1 : -1;
       }
-  )
+  );
 
   for (var c in classworkKeys) {
 
@@ -121,7 +134,7 @@ function setFromClassworkJson(json){
               function(a, b) {
                   return new Date(subject.assignments[b].datedue).getTime() - new Date(subject.assignments[a].datedue).getTime();
               }
-          )
+          );
 
           for (var s in assignmentKeys) {
               var assignment = subject.assignments[assignmentKeys[s]];
@@ -132,14 +145,72 @@ function setFromClassworkJson(json){
       i++;
   }
 
+
+  showhtml += "<div class=\"item item-divider\">Report Card</div>" +
+              "<div id=\"reportcard\"><a class=\"item item-icon-right\" onClick=\"updateReportCard()\" href=\"#\">Download<i class=\"icon super-ios-cloud-download\"></i></a></div>";
+
   document.getElementById("main").innerHTML = showhtml;
   document.getElementById("main").style.display = 'block';
   document.getElementById("loading_box").style.display = 'none';
   clearInterval(interval);
 }
 
+function setFromReportCardJSON(json){
+  var newHTML = "";
+  for(var classid in json){
+    newHTML += new ReportCardItem(json[classid].name, json[classid].averages).getHTML();
+  }
+  document.getElementById('reportcard').innerHTML = newHTML;
+}
+
 var counter;
 var interval;
+
+function startCounter(){
+  counter = 0;
+  interval = setInterval(
+      function() {
+          counter += 0.5;
+          if (counter >= 100) {
+              clearInterval(interval);
+              document.getElementById('status').innerHTML = "Something Went Wrong...";
+          } else {
+              document.getElementById('status').innerHTML = counter.toFixed(2) + "%";
+          }
+      }, 50
+  );
+}
+
+function updateReportCard(){
+
+  var username = localStorage.getItem("username").toLowerCase();
+  var password = localStorage.getItem("password");
+
+  document.getElementById("main").style.display = 'none';
+  document.getElementById("loading_box").style.display = 'block';
+
+  var formData = new FormData();
+  formData.append("password", encodeToURL(password));
+  timeout(20000, postFromAPI("homeaccess/reportcard/" + username, formData)).then(
+      function(responce) {
+          responce.json().then(
+              function(json) {
+                  clearInterval(interval);
+                  setFromReportCardJSON(json);
+                  document.getElementById("main").style.display = 'block';
+                  document.getElementById("loading_box").style.display = 'none';
+              }
+          );
+      }
+  ).catch(function(error) {
+      clearInterval(interval);
+      AppAlert("Error", "Unable to Download Report Card 😞");
+      document.getElementById("main").style.display = 'block';
+      document.getElementById("loading_box").style.display = 'none';
+  });
+
+  startCounter();
+}
 
 function updateGrades() {
 
@@ -174,18 +245,7 @@ function updateGrades() {
                 document.getElementById("loading_box").style.display = 'none';
             });
 
-            counter = 0;
-            interval = setInterval(
-                function() {
-                    counter += 0.5;
-                    if (counter >= 100) {
-                        clearInterval(interval);
-                        document.getElementById('status').innerHTML = "Something Went Wrong...";
-                    } else {
-                        document.getElementById('status').innerHTML = counter.toFixed(2) + "%";
-                    }
-                }, 50
-            );
+            startCounter();
 
         } else {
           setFromClassworkJson(JSON.parse(data));//Use Old
