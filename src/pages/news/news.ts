85950a9bf269b4e017b656b9ad201a0c1da709ef
Shrivu Shankar <shrivu1122@gmail.com>
2017-11-19 12:25:47 -0600

    Added News

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Events } from 'ionic-angular';
 
 import { Http } from '@angular/http';
+
+class Article {
+  date: string;
+  image: string;
+  link: string;
+  organization: string;
+  text: string;
+  type: number;
+}
 
 @Component({
   selector: 'page-news',
@@ -10,17 +20,36 @@ import { Http } from '@angular/http';
 export class NewsPage {
 
   newsType: string;
+  allNews: Article[];
 
-  constructor(public navCtrl: NavController, private http: Http) {
+  constructor(public navCtrl: NavController, public events: Events, private http: Http) {
 
     this.newsType = "all";
+    this.allNews = [];
 
-    this.http.get('https://cfisdapi.herokuapp.com/ping').subscribe(
+    this.events.subscribe('news:downloaded', news => {
+
+      let articles: Article[] = news.news.all;
+      articles.sort((a: Article, b: Article) => {
+        return (new Date(b.date)).getTime() - (new Date(a.date)).getTime();
+      })
+      this.allNews = articles;
+
+    });
+
+    this.http.get('http://192.168.1.65:5000/api/news/all').subscribe(
       data => {
-          alert(JSON.stringify(data));
+          this.events.publish('news:downloaded', data.json());
+      },
+      error => {
+        alert(error);
       }
     );
 
+  }
+
+  openArticle(article: Article){
+    window.open(article.link, '_system', 'location=yes');
   }
 
 }
