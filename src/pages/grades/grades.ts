5abdad835bafcce8ddc5e829d210e1a3de3dd82a
Shrivu Shankar <shrivu1122@gmail.com>
2017-11-19 13:53:57 -0600

    Integrated API into Grades

--- src/pages/grades/grades.ts
+++ src/pages/grades/grades.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Events } from 'ionic-angular';
+
+import { Http, Headers, RequestOptions } from '@angular/http';
+
+class AssignmentGrade {
+
+}
+
+class SubjectGrade {
+  name: string;
+  honors: boolean;
+  letter: string;
+  overallavg: string;
+  assignments: AssignmentGrade[];
+}
 
 @Component({
   selector: 'page-grades',
@@ -8,11 +23,53 @@ import { NavController } from 'ionic-angular';
 export class GradesPage {
 
   gradeType: string;
+  currentGrades: SubjectGrade[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public events: Events, private http: Http) {
 
     this.gradeType = "current";
 
+    this.events.subscribe('grades:current', grades => {
+
+      console.log(grades);
+
+      if(grades.status == 'success'){
+
+        let current: SubjectGrade[] = grades.grades;
+
+        this.currentGrades = current;
+
+      }
+
+    });
+
+
+    let headers = new Headers({'Content-Type' : 'application/json'});
+    let options = new RequestOptions({ headers: headers });
+
+    this.http.post('http://192.168.1.65:5000/api/classwork/s000000', JSON.stringify({password: '123456'}), options).subscribe(
+      data => {
+          this.events.publish('grades:current', data.json());
+      },
+      error => {
+        alert(error);
+      }
+    );
+
+  }
+
+  getColor(letter: string) : string {
+    if(letter == 'A'){
+      return 'great';
+    } else if(letter == 'B'){
+      return 'ok';
+    } else if(letter == 'C'){
+      return 'poor';
+    } else if(letter == 'U'){
+      return 'none';
+    } else {
+      return 'bad';
+    }
   }
 
   openClassGrades(){
