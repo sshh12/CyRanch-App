dfd62b7086988c463a29fdb9382ec6261e62f45b
Shrivu Shankar <shrivu1122@gmail.com>
2017-11-20 14:44:43 -0600

    Local Storage and Grade FAB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HttpModule } from '@angular/http';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { HttpModule } from '@angular/http';
   imports: [
     BrowserModule,
     HttpModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/grades/grades.ts
+++ src/pages/grades/grades.ts
@@ -3,6 +3,8 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Events } from 'ionic-angular';
 
 import { Http, Headers, RequestOptions } from '@angular/http';
+import { Storage } from '@ionic/storage';
+import { AlertController } from 'ionic-angular';
 
 import { Globals } from '../../app/globals';
 
@@ -30,14 +32,24 @@ export class GradesPage {
 
   gradeType: string;
   currentGrades: SubjectGrade[];
+  loading: boolean;
 
-  constructor(public navCtrl: NavController, public events: Events, private http: Http) {
+  constructor(public navCtrl: NavController,
+              public events: Events,
+              private http: Http,
+              private storage: Storage,
+              public alertCtrl: AlertController) {
 
     this.gradeType = "current";
+    this.loading = false;
 
     this.events.subscribe('grades:current', grades => {
 
       if(grades.status == 'success'){
+
+        console.log(grades);
+
+        this.storage.set('grades:data', grades);
 
         let current: SubjectGrade[] = grades.grades;
 
@@ -55,31 +67,45 @@ export class GradesPage {
 
       }
 
+      this.loading = false;
+
     });
 
-    this.loadCurrentGrades();
+    this.storage.get('grades:data').then((grades) => {
+      if(grades){
+        this.events.publish('grades:current', grades);
+      }
+    });
 
   }
 
   loadCurrentGrades(callback?){
 
-    let headers = new Headers({'Content-Type' : 'application/json'});
-    let options = new RequestOptions({ headers: headers });
+    this.loading = true;
 
-    this.http.post(Globals.SERVER + '/api/classwork/s000000', JSON.stringify({password: '123456'}), options).subscribe(
-      data => {
+    this.storage.get('grades:username').then((username) => {
+      this.storage.get('grades:password').then((password) => {
 
-          if(callback){
-            callback();
+        let headers = new Headers({'Content-Type' : 'application/json'});
+        let options = new RequestOptions({ headers: headers });
+
+        this.http.post(Globals.SERVER + '/api/classwork/' + username, JSON.stringify({password: password}), options).subscribe(
+          data => {
+
+              if(callback){
+                callback();
+              }
+
+              this.events.publish('grades:current', data.json());
+
+          },
+          error => {
+            alert(error);
           }
+        );
 
-          this.events.publish('grades:current', data.json());
-
-      },
-      error => {
-        alert(error);
-      }
-    );
+      });
+    });
 
   }
 
@@ -107,6 +133,92 @@ export class GradesPage {
     } else {
       return 'bad';
     }
+  }
+
+  showConfirmLegal(username, password) {
+
+    let confirm = this.alertCtrl.create({
+      title: 'Legal',
+      message: 'To provide assignment averages, percentiles, and other features, the app requires that you accept the policies outlined in the legal section of the app (black icon in the top right).',
+      buttons: [
+        {
+          text: 'No!',
+          handler: () => {
+            this.storage.set('grades:legal', false);
+          }
+        },
+        {
+          text: 'Yup (I accept)',
+          handler: () => {
+            this.storage.set('grades:legal', true).then(() => {
+              this.storage.set('grades:username', username).then(() => {
+                this.storage.set('grades:password', password).then(() => {
+                  this.loadCurrentGrades();
+                });
+              });
+            });
+          }
+        }
+      ]
+    });
+    confirm.present();
+
+  }
+
+  showLogin() {
+
+    let prompt = this.alertCtrl.create({
+      title: 'Login',
+      inputs: [
+        {
+          name: 'sid',
+          placeholder: 's000000'
+        }, {
+          name: 'password',
+          placeholder: 'password',
+          type: 'password'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            //
+          }
+        },
+        {
+          text: 'Go',
+          handler: data => {
+
+            this.storage.get('grades:legal').then((accepted) => {
+              if(accepted){
+                this.storage.set('grades:username', data.sid).then(() => {
+                  this.storage.set('grades:password', data.password).then(() => {
+                    this.loadCurrentGrades();
+                  });
+                });
+              } else {
+                this.showConfirmLegal(data.sid, data.password);
+              }
+            });
+
+          }
+        }
+      ]
+    });
+    prompt.present();
+
+  }
+
+  logout() {
+
+    this.storage.set('grades:username', '');
+    this.storage.set('grades:password', '');
+    this.storage.set('grades:legal', false);
+    this.storage.set('grades:data', {});
+
+    this.currentGrades = [];
+
   }
 
 }
