6ea948f7d0c2a08a3b1ed387505ed54c31c7d515
Shrivu Shankar <shrivu1122@gmail.com>
2017-11-30 19:24:50 -0600

    Implemented Grade Calc

--- src/pages/grades/calculator.ts
+++ src/pages/grades/calculator.ts
@@ -10,15 +10,63 @@ export class CalculatorPage {
 
   subject: SubjectReportCard;
   semester: string;
-  firstweeks: Number;
-  secondweeks: Number;
-  exam: Number;
-  sem: Number;
+  firstweeks: any;
+  secondweeks: any;
+  exam: any;
+  sem: any;
+  exempt: boolean;
+
+  orderOfOps: string[];
 
   constructor(public navCtrl: NavController, params: NavParams) {
     this.subject = params.data.subject;
     this.semester = 'fall';
+    this.exempt = false;
+    this.orderOfOps = ['sem', 'exam', 'second', 'first'];
     this.resetValues();
+  }
+
+  clean(num) {
+    num = "0" + num;
+    return parseFloat(num.replace(/\D/g,''));
+  }
+
+  update(section: string) {
+
+    this.orderOfOps.splice(this.orderOfOps.indexOf(section), 1);
+    this.orderOfOps.push(section);
+
+    this.firstweeks = this.clean(this.firstweeks);
+    this.secondweeks = this.clean(this.secondweeks);
+    this.exam = this.clean(this.exam);
+    this.sem = this.clean(this.sem);
+
+    let lastOp = this.orderOfOps[0];
+    if(lastOp == 'sem') {
+      if(!this.exempt){
+        this.sem = ((this.firstweeks * 2 + this.secondweeks * 2) + this.exam) / 5;
+      } else {
+        this.sem = (this.firstweeks + this.secondweeks) / 2;
+      }
+    } else if(lastOp == 'exam') {
+      if(!this.exempt){
+        this.exam = this.sem * 5 - (this.firstweeks * 2 + this.secondweeks * 2);
+      } else {
+        this.sem = ((this.firstweeks * 2 + this.secondweeks * 2) + this.exam) / 5;
+      }
+    } else if(lastOp == 'second') {
+      if(!this.exempt){
+        this.secondweeks = (this.sem * 5 - this.exam - this.firstweeks * 2) / 2;
+      } else {
+        this.secondweeks = this.sem * 2 - this.firstweeks;
+      }
+    } else {
+      if(!this.exempt){
+        this.firstweeks = (this.sem * 5 - this.exam - this.secondweeks * 2) / 2;
+      } else {
+        this.firstweeks = this.sem * 2 - this.secondweeks;
+      }
+    }
   }
 
   resetValues() {
@@ -33,6 +81,10 @@ export class CalculatorPage {
       this.exam = this.subject.exams[1].average;
       this.sem = this.subject.semesters[1].average;
     }
+    this.firstweeks = this.clean(this.firstweeks);
+    this.secondweeks = this.clean(this.secondweeks);
+    this.exam = this.clean(this.exam);
+    this.sem = this.clean(this.sem);
   }
 
 }
